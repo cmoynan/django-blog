4047b10491c65535ff42d4ff8cf9e29951d821f9
Chris Moynan <Chrismoynan@outlook.com>
2024-06-17 17:59:51 +0000

    Update settings to use SECRET_KEY from environment variables

--- codestar/settings.py
+++ codestar/settings.py
@@ -26,6 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-#++^hmfh4(97=(dg*u-*b(6@16xy!++r7u=ps7j8jk)k%l(vn!'
 
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
